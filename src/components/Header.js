4b3d24176ca9794a8be78023fd3b77042194488f
MrLaam <jordanlam60@gmail.com>
2021-11-05 14:07:13 +0000

    Created header. Set default font size and family

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import '../css/Header.css'
+import Sun from '../images/sun.svg'
 
 const Header = () => {
     return (
-        <div>
+        <div className="header-container">
+            <div className="header-title-container">
+                <h1>devfinder</h1>
+            </div>
+            
+            <div className="theme-container">
+                <a href="/">LIGHT</a>
+                <img src={Sun} alt="Sun icon"/>
+            </div>
             
         </div>
     )
