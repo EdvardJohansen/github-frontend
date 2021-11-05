16dc5bb8677e163b4ba5193b3d7a933ac86dfd83
EdvardJohansen <edvarddallj97@gmail.com>
2021-11-05 16:39:36 +0100

    Added the search component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="app-container">
         <Header />
+        <SearchBar />
     </div>
   );
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import '../css/SearchBar.css'
+import SearchIcon from '../images/search.svg'
 
 const SearchBar = () => {
     return (
-        <div>
-            
+        <div className="search-bar-container">
+            <img className="search-icon" src={SearchIcon} alt="Search icon"/>
+            <input className="search-input" type="text" placeholder="Search GitHub username"></input>
+            <button className="btn-search">Search</button>
         </div>
     )
 }
